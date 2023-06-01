e883ff631112b2f9be6e3a4b1b1e04188549d78a
Ricardogn224 <abrahamricardogn224@gmail.com>
2023-06-01 16:57:13 +0200

    team review and start of responsive version for tablette

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, send_file, render_template, request, redirect, session, url_for
+from user_agents import parse
 import os
 
 app = Flask(__name__)
 
 
+# Définir une fonction pour détecter si la résolution de l'appareil est mobile
+def is_mobile():
+    user_agent = parse(request.headers.get('User-Agent'))
+    return user_agent.is_mobile
+
+
 @app.route('/')
 def home():
+    """ if is_mobile():
+        return redirect(url_for('phone_home')) """
     return render_template('/pages/home.html')
+
+
+@app.route('/phone_home')
+def phone_home():
+
+    return render_template('/phone/phone-home.html')
+
+
+@app.route('/phone_iot')
+def phone_iot():
+    return render_template('/phone/phone_iot.html')
+
+
+@app.route('/phone_ftth')
+def phone_ftth():
+    return render_template('/phone/phone_ftth.html')
+
+
+@app.route('/phone_contact')
+def phone_contact():
+    return render_template('/phone/phone_contact.html')
 
 
 @app.route('/FTTH_pro')
@@ -60,4 +90,4 @@ def securite_ultra():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=7001)
+    app.run(host="0.0.0.0", debug=True, port=9001)
--- static/js/index.js
+++ static/js/index.js
@@ -13,3 +13,20 @@ function show_internet_option() {
     document.getElementById("id_internet_option").classList.toggle("hide");
 
 }
+
+
+const carousel = document.querySelector('.carousel');
+const slides = carousel.querySelector('.zone-carousel');
+const slideWidth = carousel.querySelector('.slide').offsetWidth;
+let currentSlide = 0;
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide > slides.children.length - 1) {
+    currentSlide = 0;
+  }
+  slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+setInterval(nextSlide, 5000);
+
